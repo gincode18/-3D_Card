2ba31963a8154d354db17217e6c9b8503172beb1
gincode18 <vishalkamboj9211@gmail.com>
2023-08-20 15:13:18 +0530

    syssiugv

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
+    
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,18 @@
 import { Environment, OrbitControls, useTexture } from "@react-three/drei";
 import * as THREE from "three";
+import { Alien } from "./Alien";
 
 export const Experience = () => {
   const map = useTexture("textures/1.jpg");
   return (
     <>
-      <ambientLight intensity={0.6}></ambientLight>
-      <Environment preset="sunset"></Environment>
+      <ambientLight intensity={1}></ambientLight>
+      {/* <Environment preset="sunset"></Environment> */}
       <OrbitControls />
+      <Alien scale={0.5} position-y={-1}> </Alien>
       <mesh>
         <sphereGeometry args={[5, 60, 60]} />
-        <meshStandardMaterial map={map} side={THREE.BackSide} />
+        <meshPhysicalMaterial map={map} side={THREE.BackSide} />
       </mesh>
     </>
   );
